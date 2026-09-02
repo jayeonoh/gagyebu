d603c0166b67854a340f1521517c945a6bb35082
jayeonoh <jayeon.oh@gmail.com>
2026-09-03 02:10:43 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.GB_API = "https://script.google.com/macros/s/AKfy…/exec";
+window.GB_API = "https://script.google.com/macros/s/AKfycbzouKVqApTQWR2vTg1aJtB2F4BG4gJWZV-0YFS_3Q_GzHauyqKOLTXWF8uktNbgoiFh8Q/exec";
