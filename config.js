5be2c0c97ef474cf0a77b140f0ff2e2c010bd5f7
jayeonoh <jayeon.oh@gmail.com>
2026-09-03 03:09:02 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.GB_API = "https://script.google.com/macros/s/AKfycbzouKVqApTQWR2vTg1aJtB2F4BG4gJWZV-0YFS_3Q_GzHauyqKOLTXWF8uktNbgoiFh8Q/exec";
+window.GB_SHEET_ID = "1wDAaL_3lFEFBJSsIxzsZ2EzDaieG8p52zBe5kRtOCEI";
