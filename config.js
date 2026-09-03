b054405e1ba4a0249c7f09f4881e6b7ede121e86
jayeonoh <jayeon.oh@gmail.com>
2026-09-03 16:01:26 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
-window.GB_API = "https://script.google.com/macros/s/AKfycbzouKVqApTQWR2vTg1aJtB2F4BG4gJWZV-0YFS_3Q_GzHauyqKOLTXWF8uktNbgoiFh8Q/exec";
+window.SB_URL = "https://sjymxfjilxqeiznvpqpq.supabase.co/rest/v1/";
+window.SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqeW14ZmppbHhxZWl6bnZwcXBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg0MDgzOTMsImV4cCI6MjEwMzk4NDM5M30.O-4XkQlkAtZJfgmJ22blWwKvaQpXpGOOVt880Zxpx9k";
 window.GB_SHEET_ID = "1wDAaL_3lFEFBJSsIxzsZ2EzDaieG8p52zBe5kRtOCEI";
